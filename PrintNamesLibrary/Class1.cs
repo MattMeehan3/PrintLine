3f50efb995169a75ce14282629a22548ddff982d
Tomab Scblieter <matt.meehan3@gmail.com>
2023-04-03 15:54:05 -0500

    Initial Commit This is the initial commit created after a 24 minute interview.

---
+++ PrintNamesLibrary/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace PrintNamesLibrary
+{
+    public class Class1
+    {
+        public static string[] PrintNumbers(int UpperBound)
+        {
+            string[] AllLines = new string[UpperBound+1];
+
+            for (int i = 1; i <= UpperBound; i++)
+            {
+                string PrintString = "";
+                if (i % 3 == 0)
+                {
+                    PrintString += "Ricky";
+                }
+                if (i % 5 == 0)
+                {
+                    PrintString += "Bobby";
+                }
+
+                if (PrintString == "")
+                {
+                    PrintString = i.ToString();
+                }
+
+                AllLines[i] = PrintString;
+            }
+
+            return AllLines;
+        }
+    }
+}
---
+++ TestPrintConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using PrintNamesLibrary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        foreach(string line in Class1.PrintNumbers(int.MaxValue))
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
