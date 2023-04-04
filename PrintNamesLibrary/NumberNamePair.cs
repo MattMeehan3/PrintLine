a8e8261a4c164d61ee459fae630084f3565366d4
Tomab Scblieter <matt.meehan3@gmail.com>
2023-04-03 22:31:28 -0500

    Add number name pair class

---
+++ PrintNamesLibrary/NumberNamePair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintNamesLibrary
+{
+    public class NumberNamePair
+    {
+        public int Number;
+        public string Name;
+
+        public NumberNamePair(int number, string name)
+        {
+            Number = number;
+            Name = name;
+        }
+    }
+}
