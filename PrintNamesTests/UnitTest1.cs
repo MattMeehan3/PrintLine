0e9c52ca4412c71b88152f27a55a6ab177a2e676
Tomab Scblieter <matt.meehan3@gmail.com>
2023-04-04 01:05:42 -0500

    Add additional tests

--- PrintNamesTests/UnitTest1.cs
+++ PrintNamesTests/UnitTest1.cs
@@ -58,6 +58,25 @@ namespace PrintNamesTests
         }
 
         [Fact]
+        public void FrankTest1()
+        {
+            //Arrange
+            int UpperBound = 10;
+            List<NumberNamePair> pairs = new List<NumberNamePair>();
+            pairs.Add(new NumberNamePair(2, "Frank"));
+            string expectedOutput = "1\r\nFrank \r\n3\r\nFrank \r\n5\r\nFrank \r\n7\r\nFrank \r\n9\r\nFrank \r\n";
+            using StringWriter writer = new();
+            Console.SetOut(writer);
+
+            //Act
+            PrintNumber.PrintNumbers(pairs, UpperBound);
+
+            //Assert
+            string actualOutput = writer.ToString();
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
         public void LisaBobbyTest1()
         {
             //Arrange
@@ -66,6 +85,44 @@ namespace PrintNamesTests
             pairs.Add(new NumberNamePair(4, "Lisa"));
             pairs.Add(new NumberNamePair(5, "Bobby"));
             string expectedOutput = "1\r\n2\r\n3\r\nLisa \r\nBobby \r\n6\r\n7\r\nLisa \r\n9\r\nBobby \r\n11\r\nLisa \r\n13\r\n14\r\nBobby \r\nLisa \r\n17\r\n18\r\n19\r\nLisa Bobby \r\n21\r\n22\r\n23\r\nLisa \r\n";
+            using StringWriter writer = new();
+            Console.SetOut(writer);
+
+            //Act
+            PrintNumber.PrintNumbers(pairs, UpperBound);
+
+            //Assert
+            string actualOutput = writer.ToString();
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void NoPairsTest101()
+        {
+            //Arrange
+            int UpperBound = 101;
+            List<NumberNamePair> pairs = new List<NumberNamePair>();
+            //Do not add any pairs to the test case.
+            string expectedOutput = "1\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9\r\n10\r\n11\r\n12\r\n13\r\n14\r\n15\r\n16\r\n17\r\n18\r\n19\r\n20\r\n21\r\n22\r\n23\r\n24\r\n25\r\n26\r\n27\r\n28\r\n29\r\n30\r\n31\r\n32\r\n33\r\n34\r\n35\r\n36\r\n37\r\n38\r\n39\r\n40\r\n41\r\n42\r\n43\r\n44\r\n45\r\n46\r\n47\r\n48\r\n49\r\n50\r\n51\r\n52\r\n53\r\n54\r\n55\r\n56\r\n57\r\n58\r\n59\r\n60\r\n61\r\n62\r\n63\r\n64\r\n65\r\n66\r\n67\r\n68\r\n69\r\n70\r\n71\r\n72\r\n73\r\n74\r\n75\r\n76\r\n77\r\n78\r\n79\r\n80\r\n81\r\n82\r\n83\r\n84\r\n85\r\n86\r\n87\r\n88\r\n89\r\n90\r\n91\r\n92\r\n93\r\n94\r\n95\r\n96\r\n97\r\n98\r\n99\r\n100\r\n101\r\n";
+            using StringWriter writer = new();
+            Console.SetOut(writer);
+
+            //Act
+            PrintNumber.PrintNumbers(pairs, UpperBound);
+
+            //Assert
+            string actualOutput = writer.ToString();
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void NoPairsTest103()
+        {
+            //Arrange
+            int UpperBound = 103;
+            List<NumberNamePair> pairs = new List<NumberNamePair>();
+            //Do not add any pairs to the test case.
+            string expectedOutput = "1\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9\r\n10\r\n11\r\n12\r\n13\r\n14\r\n15\r\n16\r\n17\r\n18\r\n19\r\n20\r\n21\r\n22\r\n23\r\n24\r\n25\r\n26\r\n27\r\n28\r\n29\r\n30\r\n31\r\n32\r\n33\r\n34\r\n35\r\n36\r\n37\r\n38\r\n39\r\n40\r\n41\r\n42\r\n43\r\n44\r\n45\r\n46\r\n47\r\n48\r\n49\r\n50\r\n51\r\n52\r\n53\r\n54\r\n55\r\n56\r\n57\r\n58\r\n59\r\n60\r\n61\r\n62\r\n63\r\n64\r\n65\r\n66\r\n67\r\n68\r\n69\r\n70\r\n71\r\n72\r\n73\r\n74\r\n75\r\n76\r\n77\r\n78\r\n79\r\n80\r\n81\r\n82\r\n83\r\n84\r\n85\r\n86\r\n87\r\n88\r\n89\r\n90\r\n91\r\n92\r\n93\r\n94\r\n95\r\n96\r\n97\r\n98\r\n99\r\n100\r\n101\r\n102\r\n103\r\n";
             using StringWriter writer = new();
             Console.SetOut(writer);
 
